cf93ecb4face7d4c7549f3fc818d8b5e032b6d52
zwkang <kang95630@gmail.com>
2023-02-28 17:04:45 +0800

    chore: remove unuse plugin alias

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,4 @@
 import { Options } from 'tsup';
-import alias from 'esbuild-plugin-alias';
-
-const paths = import.meta.url.replace(/\/[^/]+$/, '/node_modules/unfetch/dist/unfetch.mjs').replace('file://', '');
 
 const config: Options = {
   splitting: true,
@@ -10,11 +7,6 @@ const config: Options = {
   clean: true,
   dts: true,
   outDir: './dist',
-  esbuildPlugins: [
-    alias({
-      unfetch: paths,
-    }),
-  ],
 };
 
 export default config;
